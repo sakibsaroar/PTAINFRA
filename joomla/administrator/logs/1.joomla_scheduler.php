20a9228560a3d857e4bbe009b4e6e77ec86a8999
sakibsaroar <68503386+sakibsaroar@users.noreply.github.com>
2024-04-03 16:01:56 +0600

    upload

---
+++ joomla/administrator/logs/1.joomla_scheduler.php
@@ -0,0 +1,19 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2023-12-17 03:23:58 UTC
+#Software: Joomla! 5.0.1 Stable [ Kuboresha ] 28-November-2023 16:00 GMT
+
+#Fields: date	time	priority	message
+2023-12-17	03:23:58	INFO	Running task#03 'Update Notification'.
+2023-12-17	03:23:59	INFO	Successfully finished task#03 in 1.17 (net 1.17) seconds.
+2023-12-17	03:26:06	INFO	Running task#02 'Session GC'.
+2023-12-17	03:26:06	INFO	Task> SessionGC end
+2023-12-17	03:26:06	INFO	Successfully finished task#02 in 0.00 (net 0.00) seconds.
+2024-03-13	03:48:50	INFO	Running task#03 'Update Notification'.
+2024-03-13	03:48:53	INFO	Task> The mail() function has been disabled and the mail can't be sent.
+2024-03-13	03:48:53	INFO	Task> UpdateNotification end
+2024-03-13	03:48:54	INFO	Successfully finished task#03 in 1.46 (net 1.46) seconds.
+2024-03-13	03:49:19	INFO	Running task#02 'Session GC'.
+2024-03-13	03:49:19	INFO	Task> SessionGC end
+2024-03-13	03:49:20	INFO	Successfully finished task#02 in 0.00 (net 0.00) seconds.
+2024-03-13	03:49:25	INFO	Running task#01 'Rotate Logs'.
---
+++ joomla/administrator/logs/joomla_scheduler.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2024-03-13 03:49:26 UTC
+#Software: Joomla! 5.0.1 Stable [ Kuboresha ] 28-November-2023 16:00 GMT
+
+#Fields: date	time	priority	message
+2024-03-13	03:49:26	INFO	Successfully finished task#01 in 0.10 (net 0.10) seconds.
